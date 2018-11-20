6cf44fff42944d25f045df73d6d73bb93a439628
Kristin Barr <kristinbarr@gmail.com>
2018-11-20 07:35:11 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,42 @@
+
+let chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray (arr, elem) {
+  let newArr = arr.slice().unshift(elem)
+  return newArr;
+}
+
+function destructivelyAddElementToBeginningOfArray (arr, elem) {
+  arr = arr.unshift(elem)
+  return arr;
+}
+
+function addElementToEndOfArray (arr, elem) {
+  let newArr = arr.slice().push(elem);
+  return newArr;
+}
+
+function destructivelyAddElementToEndOfArray (arr, elem) {
+  arr = arr.push(elem)
+  return arr;
+}
+
+function accessElementInArray (arr, idx) {
+  return arr[idx];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (arr) {
+  return arr.splice(1);
+}
+
+function removeElementFromBeginningOfArray (arr) {
+  return arr.slice(1);
+}
+function destructivelyRemoveElementFromEndOfArray (arr) {
+  arr.pop();
+  return arr;
+}
+
+function removeElementFromEndOfArray (arr) {
+  return arr.slice(0, arr.length-1)
+}
